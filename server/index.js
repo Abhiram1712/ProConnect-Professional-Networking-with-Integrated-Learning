fd30811f9d1a68b5fdd3d559b4be30c56ccdfa9e
Abhiram1712 <poshamabhiram@gmail.com>
2026-03-31 15:34:52 +0530

    fix cors

--- server/index.js
+++ server/index.js
@@ -7,7 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: "https://pro-connect-professional-networking-mu.vercel.app"
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
